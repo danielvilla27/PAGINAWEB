736d581f2257b22c162efdf52e25d96419c26947
daniel <daniel.servinco@gmail.com>
2022-11-03 18:02:55 -0600

    practicas

--- JS/js.js
+++ JS/js.js
@@ -1,4 +1,4 @@
-var nombre;
+/* var nombre;
 var apellido;
 var num1;
 var num2;
@@ -28,3 +28,109 @@ document.write(`Muestra el resto (modulo) de dos números es:  ${resto} <br>` );
 document.write(`Muestra el resto (modulo) del primer numero dividido entre 5 es:  ${resto2} <br>` );
 document.write(`Muestra el resultado de dividir los dos números es: ${div} <br>` );
 document.write(`Muestra la suma de dos números es: ${suma} <br>` );
+ */
+
+
+/* var nombre;
+var edad;
+
+
+
+edad=parseInt(prompt("Ingrese su la edad"));
+
+
+if (edad>18) 
+{
+
+    document.write(`Su edad es: ${edad} por lo tanto es apto para conducir <br>` );
+    
+}
+
+else 
+{
+
+    document.write(`Su edad es: ${edad} por lo tanto NO es apto para conducir <br>` );
+}
+
+ */
+
+/* var nombre;
+var nota;
+
+
+
+nota=parseInt(prompt("Ingrese la nota"));
+
+
+if (nota<=3) 
+{
+
+    document.write(`Muy deficiente ${nota} <br>` );
+    
+}
+
+
+else if (nota<=5) 
+{
+
+    document.write(`Insuficiente ${nota} <br>` );
+
+}
+
+else if (nota<=6) 
+{
+
+    document.write(`suficiente ${nota} <br>` );
+
+}
+else if (nota<=7) 
+{
+
+    document.write(`Bien ${nota} <br>` );
+
+}
+else if (nota<=9) 
+{
+
+    document.write(`Notable ${nota} <br>` );
+
+}
+else if (nota>=10) 
+{
+
+    document.write(`Sobresaliente ${nota} <br>` );
+
+} */
+
+var key;
+
+key=parseInt(prompt("Ingrese un numero del 1 al 7"));
+
+
+switch (key) {
+    case 1:
+        document.write(`Lunes ${key} <br>` );
+        break;
+        case 2:
+            document.write(`Martes ${key} <br>` );
+            break;
+            case 3:
+                document.write(`Miercoles ${key} <br>` );
+                break;
+                case 4:
+                    document.write(`Jueves ${key} <br>` );
+                    break;
+                    case 5:
+                        document.write(`Viernes ${key} <br>` );
+                        break;
+                        case 6:
+                            document.write(`Sabado ${key} <br>` );
+                            break;
+                            case 7:
+                                document.write(`Domingo ${key} <br>` );
+                                break;
+
+
+    default:
+        break;
+}
